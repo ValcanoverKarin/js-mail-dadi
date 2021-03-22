21a3bc65dc4a7b40cd33d494b8db1b37be498ac0
karin valcanover <valcanoverkarin@outlook.it>
2021-03-22 17:53:52 +0100

    inizio esercizio dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,9 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//Numero random da 1 a 6, per il giocatore 
+var numeroUtente = Math.floor(Math.random() * 6 ) + 1;
+console.log(numeroUtente);
+
+//Numero random da 1 a 6, per il computer
+var numeroComputer = Math.floor(Math.random() * 6 ) + 1;
