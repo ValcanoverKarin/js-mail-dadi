3f10e254ab6846123abe7ce2e5ebafbfc2a7c226
karin valcanover <valcanoverkarin@outlook.it>
2021-03-22 17:42:41 +0100

    controllato l email era nella lista

--- mail/js/script.js
+++ mail/js/script.js
@@ -8,3 +8,15 @@ console.log(emailUtente);
 
 //lista email di chi può accedere
 var emailLista = ['email1@outlook.it', 'email2@outlook.it', 'email3@outlook.it', 'email4@outlook.it', 'email5@outlook.it']
+
+//controllare che sia nella lista di chi può accedere
+var emailTrovata = false;
+
+for (var i = 0; i < emailLista.length; i++) {
+    var emailCorrente = emailLista[i];
+    //console.log(emailCorrente)
+
+    if (emailUtente == emailCorrente) {
+        emailTrovata = true;
+    }  
+}
