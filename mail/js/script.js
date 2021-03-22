c6d5f2cc922ab54134738403dd9632cdaca286d7
karin valcanover <valcanoverkarin@outlook.it>
2021-03-22 17:28:06 +0100

    email utente piu lista e mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,3 +1,10 @@
 //Chiedi all’utente la sua email
 //controlla che sia nella lista di chi può accedere
 //stampa un messaggio appropriato sull’esito del controllo.
+
+//Chiedi all’utente la sua email
+var emailUtente = prompt ('inserisci la tua email');
+console.log(emailUtente);
+
+//lista email di chi può accedere
+var emailLista = ['email1@outlook.it', 'email2@outlook.it', 'email3@outlook.it', 'email4@outlook.it', 'email5@outlook.it']
