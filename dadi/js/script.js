ebeac90c530b7cf31af163f55d70a979ee27dc11
karin valcanover <valcanoverkarin@outlook.it>
2021-03-22 18:03:41 +0100

    fine esercizio dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,7 +3,17 @@
 
 //Numero random da 1 a 6, per il giocatore 
 var numeroUtente = Math.floor(Math.random() * 6 ) + 1;
-console.log(numeroUtente);
+console.log('numero del giocatore = ' + numeroUtente);
 
 //Numero random da 1 a 6, per il computer
 var numeroComputer = Math.floor(Math.random() * 6 ) + 1;
+console.log('numero del computer = ' + numeroComputer);
+
+//Stabilire in base al numero chi ha perso, vinto o se hanno pareggiato
+if (numeroUtente < numeroComputer) {
+    alert('Hai perso!!!');
+} else if (numeroUtente > numeroComputer) {
+    alert('Hai vinto!!!')
+} else {
+    alert('pareggio')
+}
